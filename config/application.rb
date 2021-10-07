95ad7ede20acbc2787b606d051adef79e99388e5
Matt Whiteley <matt.liam.whiteley@gmail.com>
2021-10-07 13:17:14 +0100

    Disable actionmailbox and actioncable railties to prevent the routes being added

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,23 @@
+# frozen_string_literal: true
+
 require_relative "boot"
 
-require "rails/all"
+require 'rails'
+
+# exclude action_mailbox and action_cable which are pulled in by rails/all but create unused routes
+%w[
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_text/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+].each do |railtie|
+  require railtie
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
