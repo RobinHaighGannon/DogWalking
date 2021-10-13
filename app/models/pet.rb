aadb001b27f052136b1577ec8a1b9bd1fcc85bb6
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-10-13 14:40:55 +0100

    Missed merges

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,10 +4,6 @@
 class Pet < ActiveRecord::Base
   belongs_to :customer
   has_many :newbookings, dependent: :destroy
-<<<<<<< HEAD
-  validates :name, presence: :true,
-=======
   validates :name, presence: true,
->>>>>>> main
                    length: { minimum: 2 }
 end
