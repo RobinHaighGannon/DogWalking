93314da327784b450c5b0969e8533bf5467c6636
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-10-19 10:45:17 +0100

    Make quick booking from homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,5 +6,14 @@ class HomepageController < ApplicationController
     @newbooking = Newbooking.all
     @totalbookings = @newbooking.size
     @totalmoney = @newbooking.reduce(0) { |sum, booking| sum + booking.service.price }
+    return unless params[:pet_id]
+
+    pet = Pet.find(params[:pet_id])
+    customer = pet.customer
+    make_quick_booking(pet, customer)
+  end
+
+  def make_quick_booking(pet, customer)
+    redirect_to new_customer_pet_newbooking_path(customer, pet)
   end
 end
