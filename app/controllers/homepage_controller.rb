24d66e83ee9831d7672daacda9c81b8246dd2ba4
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-10-21 09:16:35 +0100

    Implementing suggestions

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -8,12 +8,14 @@ class HomepageController < ApplicationController
     @totalmoney = @newbooking.reduce(0) { |sum, booking| sum + booking.service.price }
     return unless params[:pet_id]
 
-    pet = Pet.find(params[:pet_id])
-    customer = pet.customer
-    make_quick_booking(pet, customer)
+    make_quick_booking
   end
 
-  def make_quick_booking(pet, customer)
+  private
+
+  def make_quick_booking
+    pet = Pet.find(params[:pet_id])
+    customer = pet.customer
     redirect_to new_customer_pet_newbooking_path(customer, pet)
   end
 end
