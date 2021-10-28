86570e91abec2fcc904001145fd82d4b88baf46c
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-10-28 09:15:40 +0100

    Adding login function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 # contains all of the methods for the welcome page
 class WelcomeController < ApplicationController
-  def login; end
-
-  def index; end
+  def index
+    redirect_to homepage_index_path if params[:username] == 'Dogwalking' && params[:password] == 'pawsome'
+  end
 end
