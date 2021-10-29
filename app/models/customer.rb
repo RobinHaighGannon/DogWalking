e647857b87546254aa032536677fbf922e83370b
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-10-29 13:53:28 +0100

    Adding some tests and validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,7 @@ class Customer < ActiveRecord::Base
   has_many :pets, dependent: :destroy
   validates :name, presence: true,
                    length: { minimum: 2 }
+
+  validates_format_of :name, with: /\A\p{L}+(?:[-\s]\p{L}+)*\z/,
+                             message: 'must only contain letters, spaces and hyphens'
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,8 +3,18 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  test 'Customer name' do
+  test 'Customer should not be saved empty' do
     customer = Customer.new
     assert_not customer.save
+  end
+  test 'Customer name should not contain a symbol' do
+    customer = Customer.new
+    customer.name = 'R@£$'
+    assert_not customer.save
+  end
+  test 'Customer name should allow hyphens and spaces' do
+    customer = Customer.new
+    customer.name = 'Robin Haigh-Gannon'
+    assert customer.save
   end
 end
