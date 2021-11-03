e4b8055761cc29bb49f690a67ca1e9e3551b473b
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-11-03 17:22:04 +0000

    Fixing cops

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -32,28 +34,29 @@ ActiveRecord::Schema.define(version: 20_211_028_123_809) do
     t.boolean 'complete'
     t.string 'service_name'
     t.float 'price'
-
-  create_table "pets", force: :cascade do |t|
-    t.string "name"
-    t.string "breed"
-    t.integer "customer_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_pets_on_customer_id"
   end
 
-  create_table "services", force: :cascade do |t|
-    t.string "name"
-    t.float "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'pets', force: :cascade do |t|
+    t.string 'name'
+    t.string 'breed'
+    t.integer 'customer_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['customer_id'], name: 'index_pets_on_customer_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'services', force: :cascade do |t|
+    t.string 'name'
+    t.float 'price'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'password'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
   add_foreign_key 'newbookings', 'pets'
