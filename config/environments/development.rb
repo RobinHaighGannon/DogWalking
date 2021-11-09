2402214999a9692ea0cc6b9bd9cfd93e294c9d45
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-11-09 10:29:03 +0000

    Fixing cops

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: false
 
 require 'active_support/core_ext/integer/time'
-
+# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -77,3 +77,4 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+# rubocop:enable Metrics/BlockLength
--- db/migrate/20211109101213_add_devise_to_users.rb
+++ db/migrate/20211109101213_add_devise_to_users.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Adding encrypted passwords to users using devise gem
 class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
