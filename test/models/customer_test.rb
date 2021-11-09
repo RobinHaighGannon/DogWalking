b113c6f6833f2db42618bbe256e52d80a34411b5
Robin Haigh-Gannon <robinhaighgannon@gmail.com>
2021-11-09 09:39:57 +0000

    Implementing suggestions

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -12,27 +12,18 @@ class CustomerTest < ActiveSupport::TestCase
     assert_not customer.save
   end
   test 'Customer name should not contain a symbol' do
-    customer = create_test_customer
-    customer.name = 'R@£$'
+    customer = create_test_customer(name: 'R@£$')
     assert_not customer.save
   end
   test 'Customer name should allow hyphens and spaces' do
-    customer = create_test_customer
-    customer.name = 'Robin Haigh-Gannon'
+    customer = create_test_customer(name: 'Robin Haigh-Gannon')
     assert customer.save
   end
   test 'Customer phone number should not contain letters' do
-    customer = create_test_customer
-    customer.phone = '0823746T673Y'
+    customer = create_test_customer(phone: '0823746T673Y')
     assert_not customer.save
   end
-  def create_test_customer
-    customer = Customer.new
-    customer.name = 'Test Data'
-    customer.phone = '547839484'
-    customer.email = 'testdata@gmail.com'
-    customer.address = '3 Example Road'
-    customer.postcode = 'HG6DFR4'
-    customer
+  def create_test_customer(name: 'Test', phone: '54783', email: 'test@gmail.com', address: '3 Eg Rd', postcode: 'HG6R4')
+    Customer.new(name: name, phone: phone, email: email, address: address, postcode: postcode)
   end
 end
